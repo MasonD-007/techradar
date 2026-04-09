340b0ec44c92ac2fc61a10ec7e26e033f7fd8a60
serbakon <serbakonyt@gmail.com>
2026-04-08 18:09:48 -0700

    fix: globals.css

--- frontend/src/components/landing-page-header.tsx
+++ frontend/src/components/landing-page-header.tsx
@@ -1,5 +1,5 @@
 export default function LandingPageHeader() {
-    return (<header className="">
+    return (<header className="p-5 w-full bg-background-500">
         <h1 className="text-4xl font-bold mb-4">Welcome to TechRadar</h1>
     </header>)
 }
