0d54feed0e29eae8a3aa3a8f943f600a35df7ac9
Alex Lin <althlin@ucsc.edu>
2026-04-08 20:49:43 -0700

    feat: landing page header

--- frontend/src/components/landing-page-header.tsx
+++ frontend/src/components/landing-page-header.tsx
@@ -1,5 +1,9 @@
+import SendToRadarButton from "./send-to-radar-button";
+
 export default function LandingPageHeader() {
-    return (<header className="p-5 w-full bg-background-500">
+
+    return (<header className="p-5 w-full bg-background-700 justify-between items-center flex">
         <h1 className="text-4xl font-bold mb-4">Welcome to TechRadar</h1>
+        <SendToRadarButton />
     </header>)
 }
