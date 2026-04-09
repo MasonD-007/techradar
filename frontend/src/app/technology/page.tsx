b0c772b91faaa834ca217f7114cf43b541725912
serbakon <serbakonyt@gmail.com>
2026-04-09 08:49:20 -0700

    feat: technology skeleton

---
+++ frontend/src/app/technology/page.tsx
@@ -0,0 +1,9 @@
+export default function TechnologyPage() {
+	// TODO: Add content for the technology page
+	// TODO: use nuqs for technology data (technology/{id})
+	return (
+		<main className="min-h-screen flex flex-col items-center justify-center bg-background gap-10">
+			<div className="size-100 bg-accent" />
+		</main>
+	);
+}
