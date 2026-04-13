b78c02745733c4ddee34b2e1de59aec45e46a8c2
Alexander Lin <althlin@ucsc.edu>
2026-04-13 16:11:18 -0700

    feat: images directory

--- backend/migrations/000001_create_posts_table.down.sql
+++
@@ -1,2 +0,0 @@
-DROP INDEX IF EXISTS idx_posts_name;
-DROP TABLE IF EXISTS posts;
--- backend/migrations/000001_create_posts_table.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS posts (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    description TEXT,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
-
-CREATE INDEX IF NOT EXISTS idx_posts_name ON posts(name);
