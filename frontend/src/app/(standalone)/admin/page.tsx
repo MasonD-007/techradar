d320902aef46b78d88c1aacaf968c114f0b93b39
MasonD-007 <139420415+MasonD-007@users.noreply.github.com>
2026-04-20 16:03:12 -0700

    added standalone pages

--- frontend/src/app/(standalone)/admin/page.tsx
+++ frontend/src/app/(standalone)/admin/page.tsx
@@ -1,7 +1,7 @@
 export default function AdminPage() {
     return (
         <main className="min-h-screen flex flex-col items-center justify-center bg-background gap-10">
-            <div className="size-100 bg-accent" />
+            <h1 className="text-4xl font-bold">Admin Dashboard</h1>
         </main>
     );
 }
