2863835edf0f7aa76f05c050c84d2f87f35fafd6
serbakon <serbakonyt@gmail.com>
2026-04-30 12:48:32 -0700

    chore: removed unsued imports and commented code from admin page.tsx

--- frontend/src/app/(standalone)/admin/page.tsx
+++ frontend/src/app/(standalone)/admin/page.tsx
@@ -13,45 +13,6 @@ export default function AdminPage() {
 			</div>
 
 			<NavigationTab />
-
-			{/* <AlertDialog open={isDeleteOpen} onOpenChange={setIsDeleteOpen}>
-				<AlertDialogContent>
-					<AlertDialogHeader>
-						<AlertDialogTitle>Are you sure?</AlertDialogTitle>
-						<AlertDialogDescription>
-							{deleteTarget?.type === "blip" &&
-								`This blip is used by ${technologies.filter((t) => t.blip_id === Number(deleteTarget?.id)).length} technologies. Deleting it may affect data. `}
-							Are you sure you want to delete {deleteTarget?.name}? This action
-							cannot be undone.
-						</AlertDialogDescription>
-					</AlertDialogHeader>
-					<AlertDialogFooter>
-						<AlertDialogCancel>Cancel</AlertDialogCancel>
-						<AlertDialogAction onClick={handleDelete}>Delete</AlertDialogAction>
-					</AlertDialogFooter>
-				</AlertDialogContent>
-			</AlertDialog>
-
-			<AlertDialog
-				open={conflictDialogOpen}
-				onOpenChange={setConflictDialogOpen}
-			>
-				<AlertDialogContent>
-					<AlertDialogHeader>
-						<AlertDialogTitle>Conflicts Detected</AlertDialogTitle>
-						<AlertDialogDescription>
-							Some technologies already exist. How would you like to handle
-							them?
-						</AlertDialogDescription>
-					</AlertDialogHeader>
-					<AlertDialogFooter>
-						<AlertDialogCancel>Cancel</AlertDialogCancel>
-						<AlertDialogAction onClick={handleConfirmImport}>
-							Skip Existing
-						</AlertDialogAction>
-					</AlertDialogFooter>
-				</AlertDialogContent>
-			</AlertDialog> */}
 		</div>
 	);
 }
