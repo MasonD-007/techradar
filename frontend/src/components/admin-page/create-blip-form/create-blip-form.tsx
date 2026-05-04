31f6a3a256c8834443f12eaa580ca6479848aae9
serbakon <serbakonyt@gmail.com>
2026-05-03 17:39:28 -0700

    feat: create-blip-form skeleton

---
+++ frontend/src/components/admin-page/create-blip-form/create-blip-form.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { createFormHook, createFormHookContexts } from "@tanstack/react-form";
+import { Button } from "@/components/ui/button";
+import { createBlip } from "@/lib/actions";
+import DescriptionField from "./form-fields/description-field";
+import TechnologyField from "./form-fields/technology-field";
+
+export const { fieldContext, formContext, useFieldContext } =
+	createFormHookContexts();
+
+const { useAppForm } = createFormHook({
+	fieldContext,
+	formContext,
+	fieldComponents: {
+		TechnologyField,
+		DescriptionField,
+	},
+	formComponents: {},
+});
+
+export default function CreateBlipForm() {
+	// TODO: introduct type safety for form values
+	const form = useAppForm({
+		defaultValues: {
+			technology: "",
+			description: "",
+		},
+		onSubmit: async ({ value }) => {
+			const formData = new FormData();
+			formData.set("context", JSON.stringify(value));
+			await createBlip(formData);
+		},
+	});
+
+	return (
+		<form
+			onSubmit={(event) => {
+				event.preventDefault();
+				event.stopPropagation();
+				void form.handleSubmit();
+			}}
+			className="space-y-4"
+		>
+			<form.AppField
+				name="technology"
+				validators={{
+					onChange: ({ value }) => {
+						if (!value) {
+							return "You need to select a technology";
+						}
+						return undefined;
+					},
+				}}
+			>
+				{(field) => <field.TechnologyField label="Technology" />}
+			</form.AppField>
+
+			<form.AppField
+				name="description"
+				validators={{
+					onChange: ({ value }) => {
+						if (!value) {
+							return "Description is required";
+						}
+						return undefined; // valid
+					},
+				}}
+			>
+				{(field) => <field.DescriptionField label="Description" />}
+			</form.AppField>
+
+			<Button type="submit" className="w-full">
+				Create Blip
+			</Button>
+		</form>
+	);
+}
